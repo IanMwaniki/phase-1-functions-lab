f9bfd65b3d35e272287716cde54a0480e1abd490
Ian Maina Mwaniki <ian.mwaniki@student.moringaschool.com>
2022-08-21 15:06:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if (blocks > 42){
+        return blocks - 42;
+    }
+    else {
+        return blocks - 26;
+    }
+}
+
+function distanceFromHqInFeet(blocks){
+ return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(blocks,feet){
+    return Math.abs(feet-blocks) * 264;
+}
+
+function  calculatesFarePrice(blocks, feet){
+    const myPrice =distanceTravelledInFeet(blocks, feet);
+    if (myPrice < 400){
+        return 0;
+      }
+    else if (myPrice < 2000 && myPrice > 400){
+        return 0.02 * (myPrice - 400);
+    }
+    else if (myPrice > 2000 && myPrice <2500){
+        return 25;
+    }
+    else if (myPrice > 2500){
+        return "cannot travel that far";
+    }
+
+}
